0d8f363611657ef15718b4c3eef867fb89d1aefb
badriakkala <lcb2021029@iiitl.ac.in>
2023-09-30 19:45:57 +0530

    personality assessment incomplete - 1

---
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/PersonalityAssessmentActivity.kt
@@ -0,0 +1,14 @@
+package com.example.credit_risk_eval_badri_v01.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.credit_risk_eval_badri_v01.R
+
+class PersonalityAssessmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personality_assessment)
+    }
+
+
+}
