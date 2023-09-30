e84465d12d2c5ce5b486f5252e728cd30a7daa52
badriakkala <lcb2021029@iiitl.ac.in>
2023-09-30 23:39:23 +0530

    personalityassessmentActivity recyclerview item click enabled successfully

--- app/src/main/java/com/example/credit_risk_eval_badri_v01/adapters/PersonalityAssessmentAdapter.kt
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/adapters/PersonalityAssessmentAdapter.kt
@@ -4,9 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.credit_risk_eval_badri_v01.R
 import com.example.credit_risk_eval_badri_v01.data.PersonalityAssessmentQuestionModel
+import com.example.credit_risk_eval_badri_v01.fragments.PersonalityQuestionDesctiptionFragment
 
 class PersonalityAssessmentAdapter(private val questionsList:ArrayList<PersonalityAssessmentQuestionModel>):RecyclerView.Adapter<PersonalityAssessmentAdapter.QuestionsViewHolder>(){
     class QuestionsViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
@@ -26,5 +28,13 @@ class PersonalityAssessmentAdapter(private val questionsList:ArrayList<Personali
     override fun onBindViewHolder(holder: QuestionsViewHolder, position: Int) {
         val question = questionsList[position]
         holder.qNo.text = question.qNo
+        holder.itemView.setOnClickListener (object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val activity = p0!!.context as AppCompatActivity
+                val questionFragment = PersonalityQuestionDesctiptionFragment()
+                activity.supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,questionFragment).addToBackStack(null).commit()
+            }
+
+        })
     }
 }
