e49cd1e38942d504a9f52802b8510320e7e814d0
badriakkala <lcb2021029@iiitl.ac.in>
2023-09-30 13:59:44 +0530

    simple nav bar functionality - successful

--- app/src/main/java/com/example/credit_risk_eval_badri_v01/MainActivity.kt
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.credit_risk_eval_badri_v01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.credit_risk_eval_badri_v01.activities.HomeScreenActivity
+import com.example.credit_risk_eval_badri_v01.activities.StatusScreenActivity
+import com.example.credit_risk_eval_badri_v01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        startActivity(Intent(applicationContext, HomeScreenActivity::class.java))
+
     }
 }
