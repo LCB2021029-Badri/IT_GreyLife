da545d779e650864f5ce368f13b1b5794265b375
badriakkala <lcb2021029@iiitl.ac.in>
2023-10-01 18:04:09 +0530

    SRIT layout - positive progress - not completed -

--- app/src/main/java/com/example/credit_risk_eval_badri_v01/fragments/PersonalityQuestionDesctiptionFragment.kt
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/fragments/PersonalityQuestionDesctiptionFragment.kt
@@ -5,34 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.example.credit_risk_eval_badri_v01.R
 
 class PersonalityQuestionDesctiptionFragment : Fragment() {
+
+    private lateinit var radioGroup: RadioGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-       val rootView =  inflater.inflate(
+       val view =  inflater.inflate(
             R.layout.fragment_personality_question_desctiption,
             container,
             false
        )
 
         val data = arguments?.getString("key")
-        val questionNo:TextView = rootView.findViewById(R.id.questionNo)
+        val questionNo:TextView = view.findViewById(R.id.questionNo)
         questionNo.text = data.toString()
-
-        val questionNoFromBundle:TextView = rootView.findViewById(R.id.qnofrombundle)
+        val questionNoFromBundle:TextView = view.findViewById(R.id.qnofrombundle)
         questionNoFromBundle.text = data.toString()
 
 
+        val testing:TextView = view.findViewById(R.id.testing)
+        val option1:RadioButton = view.findViewById(R.id.option1)
+
+        //
+        radioGroup = view.findViewById(R.id.radioGroup)
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.option0 -> {
+                    Toast.makeText(requireContext(), "Hello, Option0", Toast.LENGTH_SHORT).show()
+                }
+                R.id.option1 -> {
+                    testing.text = option1.text.toString()
+                }
+                R.id.option2 -> {
+
+                }
+                R.id.option3 -> {
+
+                }R.id.option4 -> {
+
+            }
+            }
+        }
 
 
 
-        return rootView
+
+
+
+        return view
     }
+
 
 }
