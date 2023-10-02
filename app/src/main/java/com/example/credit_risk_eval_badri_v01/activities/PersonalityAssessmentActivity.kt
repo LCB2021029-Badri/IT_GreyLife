2a0958d720493cb4c940283abdba4db4c6069045
badriakkala <lcb2021029@iiitl.ac.in>
2023-10-02 20:44:49 +0530

    before trying saving

--- app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/PersonalityAssessmentActivity.kt
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/PersonalityAssessmentActivity.kt
@@ -1,5 +1,7 @@
 package com.example.credit_risk_eval_badri_v01.activities
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
@@ -7,6 +9,7 @@ import com.example.credit_risk_eval_badri_v01.R
 import com.example.credit_risk_eval_badri_v01.adapters.PersonalityAssessmentAdapter
 import com.example.credit_risk_eval_badri_v01.data.PersonalityAssessmentQuestionModel
 import com.example.credit_risk_eval_badri_v01.databinding.ActivityPersonalityAssessmentBinding
+import org.json.JSONArray
 
 class PersonalityAssessmentActivity : AppCompatActivity() {
 
@@ -32,8 +35,23 @@ class PersonalityAssessmentActivity : AppCompatActivity() {
         questionsList.add(PersonalityAssessmentQuestionModel("9"))
         questionsList.add(PersonalityAssessmentQuestionModel("10"))
 
+
+        val questions = arrayOf(
+            arrayOf("q1","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q2","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q3","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q4","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q5","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q6","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q7","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q8","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q9","opt1","opt2","opt2","opt4","opt5","null"),
+            arrayOf("q10","opt1","opt2","opt2","opt4","opt5","null"),
+        )
+
         questionsAdapter = PersonalityAssessmentAdapter(questionsList)
         recyclerView.adapter = questionsAdapter
+
     }
 
 
--- app/src/main/java/com/example/credit_risk_eval_badri_v01/fragments/PersonalityQuestionDesctiptionFragment.kt
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/fragments/PersonalityQuestionDesctiptionFragment.kt
@@ -19,44 +19,72 @@ class PersonalityQuestionDesctiptionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
        val view =  inflater.inflate(
             R.layout.fragment_personality_question_desctiption,
             container,
             false
        )
 
+
+        radioGroup = view.findViewById(R.id.radioGroup)
+        val option0:RadioButton = view.findViewById(R.id.option0)
+        val option1:RadioButton = view.findViewById(R.id.option1)
+        val option2:RadioButton = view.findViewById(R.id.option2)
+        val option3:RadioButton = view.findViewById(R.id.option3)
+        val option4:RadioButton = view.findViewById(R.id.option4)
+
+//        val questionNo:TextView = view.findViewById(R.id.questionNo)
+//        questionNo.text = data.toString()
+        val testing:TextView = view.findViewById(R.id.testing)
+//        Toast.makeText(requireContext(), "Hello, Option0", Toast.LENGTH_SHORT).show()
+7
+        //-----------------------------------------------------------------------
         val data = arguments?.getString("key")
-        val questionNo:TextView = view.findViewById(R.id.questionNo)
-        questionNo.text = data.toString()
+        val qnumber = data!!.toInt()
         val questionNoFromBundle:TextView = view.findViewById(R.id.qnofrombundle)
         questionNoFromBundle.text = data.toString()
 
+        //-----------------------------------------------------------------------
+//        val questions = arrayOf(
+//            arrayOf("q1","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q2","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q3","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q4","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q5","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q6","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q7","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q8","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q9","opt1","opt2","opt2","opt4","opt5","null"),
+//            arrayOf("q10","opt1","opt2","opt2","opt4","opt5","null"),
+//        )
 
-        val testing:TextView = view.findViewById(R.id.testing)
-        val option1:RadioButton = view.findViewById(R.id.option1)
-
-        //
-        radioGroup = view.findViewById(R.id.radioGroup)
+        //-----------------------------------------------------------------------
         radioGroup.setOnCheckedChangeListener { group, checkedId ->
             when (checkedId) {
                 R.id.option0 -> {
-                    Toast.makeText(requireContext(), "Hello, Option0", Toast.LENGTH_SHORT).show()
+                    questions[qnumber][6] = "0"
+                    testing.text = questions[qnumber][6]
                 }
                 R.id.option1 -> {
-                    testing.text = option1.text.toString()
+                    questions[qnumber][6] = "1"
+                    testing.text = questions[qnumber][6]
                 }
                 R.id.option2 -> {
-
+                    questions[qnumber][6] = "2"
+                    testing.text = questions[qnumber][6]
                 }
                 R.id.option3 -> {
-
-                }R.id.option4 -> {
-
-            }
+                    questions[qnumber][6] = "3"
+                    testing.text = questions[qnumber][6]
+                }
+                R.id.option4 -> {
+                    questions[qnumber][6] = "4"
+                    testing.text = questions[qnumber][6]
+                }
             }
         }
 
+//        testing.text = questions[qnumber][6]
 
 
 
