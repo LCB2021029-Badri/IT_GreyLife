db7da5237d0e10764c1d8f8b9dca57f7c2f268c9
badriakkala <lcb2021029@iiitl.ac.in>
2023-10-04 22:17:20 +0530

    working on client side split form here ------------------

---
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/ClientSideHomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.credit_risk_eval_badri_v01.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.credit_risk_eval_badri_v01.R
+
+class ClientSideHomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client_side_home_screen)
+    }
+}
