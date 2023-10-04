d8c6cca6b9ccdc43d5850fd76ff64727fcbff9e2
badriakkala <lcb2021029@iiitl.ac.in>
2023-10-04 16:47:26 +0530

    displaying lending status in main activity (extracted from database) implemented successfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,34 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
-    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
+//    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+//    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+    //auth
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    //realtime db
+    implementation("com.google.firebase:firebase-database-ktx")
+
+    //storage
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    //circle imageview
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    //safety net
+    implementation ("com.google.android.gms:play-services-safetynet:18.0.1")
+
+    //integrity
+    implementation ("com.google.android.play:integrity:1.2.0")
+
 }
--- app/src/main/java/com/example/credit_risk_eval_badri_v01/MainActivity.kt
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/MainActivity.kt
@@ -9,14 +9,20 @@ import com.example.credit_risk_eval_badri_v01.activities.LoginActivity
 import com.example.credit_risk_eval_badri_v01.activities.PersonalityAssessmentActivity
 import com.example.credit_risk_eval_badri_v01.activities.StatusScreenActivity
 import com.example.credit_risk_eval_badri_v01.databinding.ActivityMainBinding
+import com.example.credit_risk_eval_badri_v01.models.UserModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,12 +30,11 @@ class MainActivity : AppCompatActivity() {
 
 
         auth = Firebase.auth
-        val currentUser = auth.currentUser
-        if (currentUser == null) {
-            val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
-        }
+        database = FirebaseDatabase.getInstance()
 
+
+        checkLogin()
+        showLendingStatus()
 
         binding.btnAssessmentActivity.setOnClickListener {
             startActivity(Intent(applicationContext, PersonalityAssessmentActivity::class.java))
@@ -46,4 +51,30 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    private fun showLendingStatus(){
+        database.reference.child("users")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for(snapshot1 in snapshot.children){
+                        val user = snapshot1.getValue(UserModel::class.java)
+                        if(user!!.uid == FirebaseAuth.getInstance().uid){
+                            binding.tvLending.text = user!!.lender
+                        }
+                    }
+
+                }
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+    }
+
+    private fun checkLogin(){
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
