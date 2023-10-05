7bae1231658e654bffd4efd729c116980aa874d1
badriakkala <lcb2021029@iiitl.ac.in>
2023-10-05 17:33:05 +0530

    borrower home screen loan type recycler view successfully implemented

--- app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/HomeScreenActivity.kt
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/HomeScreenActivity.kt
@@ -3,12 +3,21 @@ package com.example.credit_risk_eval_badri_v01.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.credit_risk_eval_badri_v01.R
+import com.example.credit_risk_eval_badri_v01.adapters.LoanTypeAdapter
+import com.example.credit_risk_eval_badri_v01.adapters.PersonalityAssessmentAdapter
+import com.example.credit_risk_eval_badri_v01.data.LoanTypeModel
+import com.example.credit_risk_eval_badri_v01.data.PersonalityAssessmentQuestionModel
 import com.example.credit_risk_eval_badri_v01.databinding.ActivityHomeScreenBinding
 
 class HomeScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeScreenBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var loanTypeAdapter: LoanTypeAdapter
+    private lateinit var loanTypeList: ArrayList<LoanTypeModel>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +26,14 @@ class HomeScreenActivity : AppCompatActivity() {
 
         enableBottomNavView()
 
+        recyclerView = binding.recyclerView
+        loanTypeList = ArrayList()
+        loanTypeList.add(LoanTypeModel("Tractor", R.drawable.ic_tractor))
+        loanTypeList.add(LoanTypeModel("2-Wheeler", R.drawable.ic_scooter))
+        loanTypeList.add(LoanTypeModel("Used Cars", R.drawable.ic_car))
+        loanTypeList.add(LoanTypeModel("3-Wheeler", R.drawable.ic_auto))
+        loanTypeAdapter = LoanTypeAdapter(loanTypeList,this)
+        recyclerView.adapter = loanTypeAdapter
 
 
     }
---
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/adapters/LoanTypeAdapter.kt
@@ -0,0 +1,84 @@
+package com.example.credit_risk_eval_badri_v01.adapters
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.credit_risk_eval_badri_v01.R
+import com.example.credit_risk_eval_badri_v01.activities.HomeScreenActivity
+import com.example.credit_risk_eval_badri_v01.data.LoanTypeModel
+import com.example.credit_risk_eval_badri_v01.fragments.BorrowerLoanDetailsFragment
+import com.example.credit_risk_eval_badri_v01.fragments.PersonalityQuestionDesctiptionFragment
+
+class LoanTypeAdapter(private val loanTypeList:ArrayList<LoanTypeModel>, private val activity: HomeScreenActivity):
+    RecyclerView.Adapter<LoanTypeAdapter.LoanTypeViewHolder>() {
+
+    class LoanTypeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val tvLoanType: TextView = itemView.findViewById(R.id.tvLoanType)
+        val ivLoanType: ImageView = itemView.findViewById(R.id.ivLoanType)
+        val layout:LinearLayout = itemView.findViewById(R.id.layout)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LoanTypeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rv_loan_type_item,parent,false)
+        return LoanTypeViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return loanTypeList.size
+    }
+
+    var index = -1
+    override fun onBindViewHolder(holder: LoanTypeViewHolder, position: Int) {
+        val loanType = loanTypeList[position]
+        holder.tvLoanType.text = loanType.loanType
+        holder.ivLoanType.setImageResource(loanType.loanIcon)
+        holder.itemView.setOnClickListener (object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val activity = p0!!.context as AppCompatActivity
+                val borrowerLoanDetailsFragment = BorrowerLoanDetailsFragment()
+
+                //try
+                val bundle = Bundle()
+                bundle.putString("key2", loanType.loanType)
+                borrowerLoanDetailsFragment.arguments = bundle
+
+                activity.supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView,borrowerLoanDetailsFragment)
+                    .addToBackStack(null)
+                    .commit()
+
+                index = position
+                notifyDataSetChanged()
+            }
+
+        })
+
+        if(index == position){
+            holder.tvLoanType.setTextColor(Color.BLACK)
+            holder.layout.setBackgroundColor(Color.parseColor("#FDD962"))
+
+            val color = ContextCompat.getColor(holder.itemView.context, R.color.black)
+            holder.ivLoanType.setColorFilter(color)
+
+        }
+        else{
+            holder.tvLoanType.setTextColor(Color.parseColor("#C6C7C9"))
+            holder.layout.setBackgroundColor(Color.parseColor("#233238"))
+
+            val color = ContextCompat.getColor(holder.itemView.context, R.color.myWhite)
+            holder.ivLoanType.setColorFilter(color)
+
+        }
+
+    }
+
+}
