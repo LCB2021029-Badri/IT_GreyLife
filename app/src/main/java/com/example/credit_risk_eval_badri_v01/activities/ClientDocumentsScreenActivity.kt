d78561557551486ad853017cb805fc8b3beaab0a
badriakkala <lcb2021029@iiitl.ac.in>
2023-10-09 02:51:08 +0530

    docs rv and docs details added to lender side

--- app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/ClientDocumentsScreenActivity.kt
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/ClientDocumentsScreenActivity.kt
@@ -4,17 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.credit_risk_eval_badri_v01.R
+import com.example.credit_risk_eval_badri_v01.adapters.DocsAdapter
 import com.example.credit_risk_eval_badri_v01.databinding.ActivityClientDocumentsScreenBinding
+import com.example.credit_risk_eval_badri_v01.models.ChatAdapter
+import com.example.credit_risk_eval_badri_v01.models.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ClientDocumentsScreenActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityClientDocumentsScreenBinding
+    private lateinit var database: FirebaseDatabase
+    private lateinit var userList : ArrayList<UserModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityClientDocumentsScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         enableBottomNavView()
+        getUserListFromDatabaseAndSetToRecyclerView()
 
     }
 
@@ -40,5 +51,34 @@ class ClientDocumentsScreenActivity : AppCompatActivity() {
             }
         }
     }
+
+
+    private fun getUserListFromDatabaseAndSetToRecyclerView(){
+        database = FirebaseDatabase.getInstance()
+        userList = ArrayList()
+
+        database.reference.child("users")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    userList.clear()
+                    for(snapshot1 in snapshot.children){
+                        val user = snapshot1.getValue(UserModel::class.java)
+                        if(user!!.uid != FirebaseAuth.getInstance().uid){
+                            userList.add(user)
+                        }
+                    }
+
+                    //update the RV adapter with the new userList if data changes
+                    binding.userListRecyclerView.adapter = DocsAdapter(this@ClientDocumentsScreenActivity,userList)
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+    }
+
 
 }
---
+++ app/src/main/java/com/example/credit_risk_eval_badri_v01/activities/DocsDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.credit_risk_eval_badri_v01.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.credit_risk_eval_badri_v01.R
+
+class DocsDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_docs_details)
+    }
+}
